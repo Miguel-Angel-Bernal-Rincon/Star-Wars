cb235202f4b9b000cb8373eee3a1cb0f0cb04b58
Miguel-Angel-Bernal-Rincon <55267229+Miguel-Angel-Bernal-Rincon@users.noreply.github.com>
2019-10-12 23:06:33 -0500

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,3 +10,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Star Wars';
 }
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { HttpClientModule} from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PersonajesComponent } from './personajes/personajes.component';
-import { PeliculasComponent } from './peliculas/peliculas.component';
-import { PlanetasComponent } from './planetas/planetas.component';
-import { NavesComponent } from './naves/naves.component';
-import { VehiculosComponent } from './vehiculos/vehiculos.component';
-import { EspeciesComponent } from './especies/especies.component';
+import { PersonajesComponent } from './Componentes/personajes/personajes.component';
+import { PeliculasComponent } from './Componentes/peliculas/peliculas.component';
+import { PlanetasComponent } from './Componentes/planetas/planetas.component';
+import { NavesComponent } from './Componentes/naves/naves.component';
+import { VehiculosComponent } from './Componentes/vehiculos/vehiculos.component';
+import { EspeciesComponent } from './Componentes/especies/especies.component';
+import { RoutingModule } from './routing/routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './Componentes/home/home.component';
+
 
 @NgModule({
   declarations: [
@@ -20,14 +33,28 @@ import { EspeciesComponent } from './especies/especies.component';
     PlanetasComponent,
     NavesComponent,
     VehiculosComponent,
-    EspeciesComponent
+    EspeciesComponent,
+    NavBarComponent,
+    HomeComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    HttpClientModule,
+    FormsModule,
+    RoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    FlexLayoutModule,
+    RouterModule,
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
